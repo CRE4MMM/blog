4facb7c129a2c994773846d6e5a057017a61120e
CRE4MMM <haidariqq90@gmail.com>
2025-03-13 03:18:22 +0700

    akhire mari

--- src/app/detail/[slug]/page.tsx
+++ src/app/detail/[slug]/page.tsx
@@ -1,36 +1,56 @@
 import { apiCall } from '@/utils/apiHelpers'
 import Image from 'next/image'
 
-export default async function BlogDetail({
-    params,
-}: {
-    params: { slug: string }
-}) {
-    try {
-    console.log('Fetching post with slug:', params.slug) // Log slug yang digunakan
-
-    const response = await apiCall.get(`/post/${params.slug}`)
-    console.log('API Response:', response) // Log response dari API
-
-    const post = response.data
-    console.log('Post Data:', post) // Log data yang diterima
-
-    return (
-        <div className="max-w-2xl mx-auto p-6 mt-15">
-            <h1 className="text-2xl font-bold mb-4">{post.title}</h1>
-            <Image
-            src={post.thumbnail}
-            alt={post.title}
-            width={300}
-            height={200}
-            className="w-full h-64 object-cover mb-4"
-            />
-            <p className="text-gray-700">{post.blog}</p>
-        </div>
-    )
-    } catch (error) {
-        console.log('Error fetching post:', error) // Log error jika terjadi masalah
-
-        return <h1 className="text-center text-red-500">Blog not found</h1>
-    }
+interface IBlog {
+  thumbnail: string
+  title: string
+  blog: string
+  objectId: string
+  created: string
+  accountData: {
+    firstname: string
+    lastname: string
+    email: string
+  }
 }
+
+const getBlogDetail = async (slug: string) => {
+  try {
+    const query = encodeURIComponent(`objectId='${slug}'`)
+    const response = await apiCall.get(
+      `/post?where=${query}&loadRelations=accountData`
+    )
+    return response.data[0]
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const BlogDetail = async ({
+  params,
+}: {
+  params: Promise<{ slug: string}>
+}) => {
+  const slug = (await params).slug
+  const blog: IBlog = await getBlogDetail(slug)
+  return (
+    <div className='max-w-2xl mx-auto p-6 mt-15'>
+      <h1 className='text-2xl font-bold mb-4'>
+        {blog.title}
+      </h1>
+      <Image
+        src={blog.thumbnail}
+        alt={blog.title}
+        width={300}
+        height={200}
+        className='w-full h-64 object-cover mb-4'
+      />
+      <p className='text-black'>
+        {blog.blog}
+      </p>
+    </div>
+  )
+}
+
+
+export default BlogDetail
